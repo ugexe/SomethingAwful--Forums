d880ccf9b0c0d85f63ac9d240ed505a48d74db17
ugexe <nlogan@gmail.com>
2014-03-13 17:48:23 -0400

    Added thread sniping example

---
+++ examples/snipe.pl
@@ -0,0 +1,69 @@
+use Modern::Perl;
+use Getopt::Long::Descriptive;
+use lib '../lib';
+use Number::Range;
+use SomethingAwful::Forums;
+
+# Snipe somethingawful threads.
+
+my ($opt, $usage) = describe_options(
+  "$0 %o",
+    [ 'username|u=s',   'your username maybe?',       { required => 1  }, ],
+    [ 'password|p:s',   'hmmmmm',                     { required => 1  }, ],
+    [ 'forum_id|f:i',   'thread_id to use',           { required => 1  }, ],
+    [ 'pages|pg:s',     'pages of forum to use',      { default  => 1  }, ],
+    [ 'max|m:i',         'Max snipes (0 = no limit)', { default  => 5  }, ],
+    [ 'limited',        'Only snipe pages 2,3,and 69',                    ],
+    [],
+    [ 'help', 'print usage message and exit'                              ],
+);
+if( $opt->help ) {
+    say $usage->text; 
+    exit; 
+}
+
+my @pages = Number::Range->new($opt->pages)->range;
+
+say 'Starting...';
+
+my $SA = SomethingAwful::Forums->new;
+$SA->login(
+    'username' => $opt->username,
+    'password' => $opt->password,
+);
+my $scraped_forum = $SA->fetch_threads( forum_id => $opt->forum_id, pages => \@pages );
+
+# Allows breaking out of 2 loops while declaring state $counter on an inner loop
+CRAWLER: {
+    foreach my $forum_page ( @{ $scraped_forum } ) {
+        foreach my $thread ( @{ $forum_page->{threads} } ) {
+            next if ($thread->{counts}->{reply} + 1) % 40;
+
+            my $snipe = ':sicknasty:';
+            my $next_page = ($thread->{counts}->{page} + 1);
+
+            if( $next_page == 2 ) {
+                $snipe = ':synpa:';
+            }
+            elsif( $next_page == 3 ) {
+                $snipe = ':page3:';
+            }
+            elsif( $next_page == 69 ) {
+                $snipe = ':69snypa:';
+            }
+            else {
+                next if $opt->limited;
+            }
+
+            $SA->reply_to_thread( $forum_page->{id}, $snipe )
+            say 'Sniped: ' . $snipe . ' | ' . $thread->{title};
+            state $counter++;
+            last CRAWLER if( $counter != 0 && $counter >= $opt->max );
+        }
+    }
+}
+
+1;
+
+
+__END__
